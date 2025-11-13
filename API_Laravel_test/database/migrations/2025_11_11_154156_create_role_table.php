c7f2d5b39bd3d65095820e069fd0c3e155fe482d
semen <kuzubovvsemen@gmail.com>
2025-11-13 19:30:15 +0500

    User + migrations

---
+++ API_Laravel_test/database/migrations/2025_11_11_154156_create_role_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 255)->unique();
+            $table->string('name', 255);
+            $table->string('permissions', 255);
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role');
+    }
+}
